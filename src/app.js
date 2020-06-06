c69ad355eda5927ca1c379f44b937476dd21e9c9
Akash <akashmenon11699cool@hotmail.com>
2020-06-06 17:27:38 +0530

    app

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+require('./db/connect');
+
+const userRouter = require('./routers/userRouter');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerHelper('ifEquals', function(arg1, arg2) {
+    return (arg1 == arg2);
+});
+
+const SESS_SECRET = process.env.SESS_SECRET;
+const SESS_ID = process.env.SESS_ID;
+const TWO_HOURS = 1000 * 60 * 60 * 2;
+
+app.use(session({
+    name: SESS_ID,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESS_SECRET,
+    cookie: {
+        maxAge: TWO_HOURS,
+        sameSite: true,
+        secure: false,
+    }
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+
+app.use(userRouter);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port:${PORT}`);
+});
